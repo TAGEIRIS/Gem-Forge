e8a5a46c99ffce612ceeff834cd12c55086aef91
TAGEIRIS <3356627004@qq.com>
2026-09-02 16:32:43 +0800

    Gemmanager架构图制作完毕

--- Assets/_Project/Script/Config/GemConfig.cs
+++ Assets/_Project/Script/Config/GemConfig.cs
@@ -8,15 +8,12 @@ public class GemConfig : ScriptableObject
 {
     public string Id;
     public string displayName;
+    //宝石图标
     public Sprite icon;
     //宝石类型
     public GemType gemType;
     public bool isActive;          // true=主动按键触发，false=被动自动触发
     public GameObject GemPrefab;
-    //出售价格
-    public int SellingPrice;
-    //购入价格
-    public int BuyingPrice;
     // 子弹配置ID
     public string GemProjectileId;
     //宝石简介
